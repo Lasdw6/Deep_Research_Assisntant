f95d22e741fc201371f2f1740f68b4a8d0ffabac
VIvidh Mahajan <vividhmahajan@gmail.com>
2025-05-20 11:03:28 +0530

    updated prompt

--- agent.py
+++ agent.py
@@ -136,7 +136,8 @@ NEVER fake or simulate tool output yourself. If you are unable to make progreess
 ... (this Thought/Action/Observation cycle can repeat as needed) ...
 Thought: I now know the final answer
 Final Answer: YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. If you are asked for a number, don't use comma to write your number neither use units such as $ or percent sign unless specified otherwise. If you are asked for a string, don't use articles, neither abbreviations (e.g. for cities), and write the digits in plain text unless specified otherwise. If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the list is a number or a string. For one word answers, start with the word with a capital letter.
-Make sure to follow any formatting instructions given by the user.
+Make sure to follow any formatting instructions given by the user. 
+Note: If you are unable arrive at a final answer, summarize the information you have gathered such as links and provide a final answer. Also state a reason for you not being able to answer the question.
 Now begin! Reminder to ALWAYS use the exact characters `Final Answer:` when you provide a definitive answer. DO NOT USE TAVILY SEARCH FOR CLARIFICATION OR BASIC QUESTIONS THAT YOU CAN ANSWER USING YOUR INTERNAL KNOWLEDGE"""
 
 # Generate the chat interface, including the tools
